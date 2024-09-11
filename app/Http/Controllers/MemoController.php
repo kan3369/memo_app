114ad16238f94dc44be7f425e1431c4570f4ef9b
kan2269 <watari.tls@gmail.com>
2024-09-11 16:22:53 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    //
+    //一覧ページ
+    public function index()
+    {
+        $memos = Memo::all();
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    //紹介ページ
     public function show($id)
     {
         $memo = Memo::find($id);
