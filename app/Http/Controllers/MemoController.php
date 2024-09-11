a543f533d37e3d442ae937b5189a7317c66dd728
kan2269 <watari.tls@gmail.com>
2024-09-11 17:41:37 +0900

    登録機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -14,6 +14,26 @@ class MemoController extends Controller
         return view('memos.index', ['memos' => $memos]);
     }
 
+    //登録ページ
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    //登録フォームの送信先のページ
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo;
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+        // インスタンスに値を設定して保存
+        $memo->save();
+        // 登録したらindexに戻る
+        return redirect(route('memos.index'));
+    }
+
     //紹介ページ
     public function show($id)
     {
